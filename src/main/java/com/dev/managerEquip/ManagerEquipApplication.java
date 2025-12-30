792e2456b0ba797c4884da77fef0c4b907214a7e
helpdesk - pc <contatoluansantos7@gmail.com>
2025-12-30 10:33:02 -0300

    feat: iniciando o projeto.

---
+++ src/main/java/com/dev/managerEquip/ManagerEquipApplication.java
@@ -0,0 +1,13 @@
+package com.dev.managerEquip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerEquipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerEquipApplication.class, args);
+	}
+
+}
